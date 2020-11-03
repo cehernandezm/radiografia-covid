622c46c794f85a9e7e1e1313473616c37d59018e
Carlos Hernandez <carloshernandez.ch779@gmail.com>
2020-11-03 12:25:56 -0600

    Api que consulte el modelo y devuelva la prediccion

--- api/index.py
+++ api/index.py
@@ -2,52 +2,58 @@ import os
 from flask import Flask,jsonify,request
 import json
 from flask_cors import CORS, cross_origin
-
+import tensorflow as tf
+from tensorflow import keras
+import base64
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np 
 
 app = Flask(__name__)
 app.debug = True 
 CORS(app)
-
+modelo = tf.keras.models.load_model(os.path.abspath(os.getcwd()) + '/api/keras_model/modelo')
 
 
 
 ################################################### RUTAS ##############################################
 
-@app.route("/",methods=['GET'])
+@app.route("/",methods=['POST'])
 def index():
-    data = abrirArchivo(direccion + 'cpu-module')
-    if (data == 0):
-        response = jsonify({'status':400, 'data':'no existe un archivo de informacion para el cpu'})
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+    imagen = request.json['imagen']
+    database64 = (imagen.split(sep="base64,"))[1]
+    img = base64.b64decode(database64)
     
-    data = json.loads(data)
-    hz = data["HZ"]
-    total = data["tiempo"]
-    segundos = data["segundos"]
-
-    porcentaje = ((total / hz) / segundos) * 100
-
-    #print(data)
-
-    dataMemoria = abrirArchivo(direccion + 'memoria-module')
-    if (dataMemoria == 0):
-            response = jsonify({'status':400, 'data':'no existe un archivo de informacion para la memoria'})
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return response
+    #filename = os.path.abspath(os.getcwd()) + '/api/images/covid.jpg'  # I assume you have a way of picking unique filenames
+    #with open(filename, 'wb') as f:
+    #    f.write(img)
     
-    dataMemoria = json.loads(dataMemoria)
-    #print(dataMemoria)
+    labels = ["COVID-19", "NORMAL", "Viral Pneumonia"]
     
+    image_custom = ImageDataGenerator(
+        rescale=1./255
+    )
+    test_dir = os.path.abspath(os.getcwd()) + '/api/upload'
+    print (test_dir)
+    generatorCustom = image_custom.flow_from_directory(
+        directory=test_dir,
+        batch_size=1,
+        shuffle=False
+    )
+
+    y_prob = modelo.predict(generatorCustom)
+    y_pred = np.argmax(y_prob)
+
+    print(y_prob)
+    print(y_pred)
+
     response = jsonify({
-        'status':200, 
-        'cpu': porcentaje,
-        'total' : dataMemoria['total'],
-        'libre' : dataMemoria['libre'],
-        'ram' : dataMemoria['uso']
-        })
+            'status':200, 
+            'msg': "ajksdhkjas"
+            })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+        
+
 
 
 
